3dd7fd6d0e545e83fb060d175dee2476c820aa7f
Josh Kinney <joshkinney@kinneykare.com>
2020-01-23 19:25:15 -0600

    added text field to save in the PINNED location and also set up the ability to pin locations

--- Map That Spot/ViewController.swift
+++ Map That Spot/ViewController.swift
@@ -13,6 +13,8 @@ import CoreLocation
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate  {
 
     @IBOutlet var mapView: MKMapView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var noteText: UITextField!
     
     var locationManager = CLLocationManager()
     
@@ -25,20 +27,42 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
         
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 2
+        mapView.addGestureRecognizer(gestureRecognizer)
     }
+    
+    
+    @objc func chooseLocation(gestureRecognizer: UILongPressGestureRecognizer) {
+        
+        //if the tap gesture began, do this
+        if gestureRecognizer.state == .began {
+            
+            let touchPoint = gestureRecognizer.location(in: self.mapView)
+            let touchedCoordinates = self.mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchedCoordinates
+            annotation.title = nameText.text
+            annotation.subtitle = noteText.text
+            self.mapView.addAnnotation(annotation)
+        }
+        
+        
+    }
+    
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         let coordinate = locations[0].coordinate
         
         let location = CLLocationCoordinate2DMake(coordinate.latitude, coordinate.longitude)
-        let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+        let span = MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03)
         let region = MKCoordinateRegion(center: location, span: span)
         
         mapView.setRegion(region, animated: true)
         
     }
-
-
+    
 }
 
